56d54fdfd7d8d820eb947df9fd9cbbdf61d403c9
Your Name <you@example.com>
2026-05-20 14:03:54 +0000

    Avance

--- src/controllers/BrandController.java
+++ src/controllers/BrandController.java
@@ -1,5 +1,26 @@
 package controllers;
 
+import models.Brand;
+
 public class BrandController {
 
+  public Brand[] sortBubbleDesc(Brand[] brands) {
+    // Ordenamiento de arreglo.
+    for (int i = 0; i < brands.length - 1; i++) {
+      boolean swap = false;
+      for (int j = 0; j < brands.length - i - 1; j++) {
+        if (brands[j].getTotalVaildYears() < (brands[j + 1].getTotalVaildYears())) {
+          Brand temp = brands[j];
+          brands[j] = brands[j + 1];
+          brands[j + 1] = temp;
+          swap = true;
+        }
+
+      }
+      if (!swap) {
+        break;
+      }
+    }
+    return brands;
+  }
 }
--- src/models/CarYear.java
+++ src/models/CarYear.java
@@ -2,4 +2,35 @@ package models;
 
 public class CarYear {
 
+  private int year;
+  private boolean isVaild;
+
+  public CarYear() {
+  }
+
+  public CarYear(int year, boolean isVaild) {
+    this.year = year;
+    this.isVaild = isVaild;
+  }
+
+  public int getYear() {
+    return year;
+  }
+
+  public void setYear(int year) {
+    this.year = year;
+  }
+
+  public boolean isVaild() {
+    return isVaild;
+  }
+
+  public void setVaild(boolean isVaild) {
+    this.isVaild = isVaild;
+  }
+
+  @Override
+  public String toString() {
+    return "CarYear [year=" + year + ", isVaild=" + isVaild + "]";
+  }
 }
